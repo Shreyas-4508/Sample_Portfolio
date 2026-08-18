b7bc3860ac2adf2f6a0d8438f7b86d72d609c6d7
Shreyas <shreyasbari5@gmail.com>
2026-08-19 00:05:27 +0530

    Files to deploy

---
+++ Shreyas_Files/script.js
@@ -0,0 +1,10 @@
+const userResponse = confirm("Do you want to enter");
+
+if (userResponse) {
+    console.log("User clicked Yes");
+    // Add your code here for Yes action
+} else {
+    console.log("User clicked No");
+    window.close();
+    // Add your code here for No action
+}
